a7a840ae3df08f4ec2ce1e5dedd035203aa80b10
Jeremi M Gosney <jgosney@terahash.com>
2019-01-22 23:28:02 -0600

    support setting bcrypt cost from yaml config

--- lib/Dancer2/Plugin/Passphrase/Core.pm
+++ lib/Dancer2/Plugin/Passphrase/Core.pm
@@ -72,8 +72,11 @@ sub _merge_options {
     if ( $algorithm eq 'Bcrypt' ) {
         $settings->{'scheme'} = 'CRYPT';
         $settings->{'type'}   = '2a';
-        $settings->{'cost'} =
-          defined $settings->{'cost'} ? $settings->{'cost'} : 4;
+
+        unless ( defined $settings->{'cost'} ) {
+            $settings->{'cost'} = $self->{'Bcrypt'}{'cost'} || 4;
+        }
+
         $settings->{'cost'}   = 31 if $settings->{'cost'} > 31;
         $settings->{'cost'}   = sprintf '%02d', $settings->{'cost'};
     }
